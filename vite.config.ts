65cb9b200981a6b6dcdded82f3eabe22eedc51f3
44tl <urbuina@gmail.com>
2026-08-18 04:42:12 +0300

    serve static website from root and isolate desktop app to app folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from "@vitejs/plugin-react";
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
+  root: "./app",
   plugins: [react()],
   clearScreen: false,
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   server: {
     port: 1420,
     strictPort: true,
